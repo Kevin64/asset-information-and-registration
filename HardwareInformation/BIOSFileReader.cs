97a52ec56dcb2065b75eaabf11846b9f0507be5d
Kevin Costa <kevinkentuxo@hotmail.com>
2021-04-14 09:23:36 -0300

    Bug fix
    - Fixed a bug where PC brand and model were not being detected correctly for the BIOS update alert, due to a condition check not being executed correctly.

--- HardwareInformation/BIOSFileReader.cs
+++ HardwareInformation/BIOSFileReader.cs
@@ -10,15 +10,21 @@
 
             while ((line = file.ReadLine()) != null)
             {
-                if((brand.Contains("Hewlett-Packard") || brand.Contains("Quanta")) && line.Substring(0, line.IndexOf("-")).Equals("HP"))
-                    if(model.Contains(line.Substring(line.IndexOf("-") + 1, line.IndexOf("=") - line.IndexOf("-") - 1)))
-                        return line.Substring(line.IndexOf("=") + 1);
-                else if(brand.Contains("Dell") && line.Substring(0, line.IndexOf("-")).Equals("Dell"))
+                if ((brand.Contains("Hewlett-Packard") || brand.Contains("Quanta")) && line.Substring(0, line.IndexOf("-")).Equals("HP"))
+                {
                     if (model.Contains(line.Substring(line.IndexOf("-") + 1, line.IndexOf("=") - line.IndexOf("-") - 1)))
                         return line.Substring(line.IndexOf("=") + 1);
-                else if(brand.Contains("Lenovo") && line.Substring(0, line.IndexOf("-")).Equals("Lenovo"))
+                }
+                else if (brand.Contains("Dell") && line.Substring(0, line.IndexOf("-")).Equals("Dell"))
+                {
                     if (model.Contains(line.Substring(line.IndexOf("-") + 1, line.IndexOf("=") - line.IndexOf("-") - 1)))
                         return line.Substring(line.IndexOf("=") + 1);
+                }
+                else if (brand.Contains("Lenovo") && line.Substring(0, line.IndexOf("-")).Equals("Lenovo"))
+                {
+                    if (model.Contains(line.Substring(line.IndexOf("-") + 1, line.IndexOf("=") - line.IndexOf("-") - 1)))
+                        return line.Substring(line.IndexOf("=") + 1);
+                }
             }
             file.Close();
             return null;
