f93943911ed5ab8c8632969a8d1f721b7208e5b5
Kevin Costa <kevinkentuxo@hotmail.com>
2021-10-01 11:15:53 -0300

    Version update
    - Version update.

--- HardwareInformation/Properties/AssemblyInfo.cs
+++ HardwareInformation/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.7.4.2109")]
-[assembly: AssemblyFileVersion("3.7.4.2109")]
+[assembly: AssemblyVersion("3.7.4.2110")]
+[assembly: AssemblyFileVersion("3.7.4.2110")]
