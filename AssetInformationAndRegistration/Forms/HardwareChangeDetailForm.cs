483e32c67277d73f12bb546bc057501a99e2ab39
Kevin Costa <kevinvtcosta@outlook.com>
2023-11-08 16:16:45 -0300

    Update HardwareChangeDetailForm.cs

--- AssetInformationAndRegistration/Forms/HardwareChangeDetailForm.cs
+++ AssetInformationAndRegistration/Forms/HardwareChangeDetailForm.cs
@@ -3,14 +3,10 @@ using ConstantsDLL.Properties;
 using LogGeneratorDLL;
 using RestApiDLL;
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Management.Instrumentation;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AssetInformationAndRegistration.Forms
@@ -18,22 +14,54 @@ namespace AssetInformationAndRegistration.Forms
     public partial class HardwareChangeDetailForm : Form, ITheming
     {
         private readonly LogGenerator log;
+        private readonly List<string> attributes = new List<string>() { "processor", "ram", "storage", "videoCard", "cpu_id", "name", "frequency", "numberOfCores", "numberOfThreads", "cache", "amount", "manufacturer", "type", "serialNumber", "type", "connection", "model", "serialNumber", "size", "smartStatus", "storageId", "gpuId", "vRam" };
 
         public HardwareChangeDetailForm(LogGenerator log)
         {
             InitializeComponent();
             FormClosing += HardwareChangeForm_Closing;
             this.log = log;
+
         }
 
-        public void Recursion<T>(TreeNode root, T hw)
+        public void PopulateTreeView<T>(TreeNode root, T item)
         {
-            foreach (PropertyInfo pi in hw.GetType().GetProperties())
+            TreeNode childNode;
+            try
             {
-                var childNode = root.Nodes[root.Nodes.Add(new TreeNode(pi.Name))];
-                childNode.Tag = pi;
-                Recursion(childNode, pi.GetValue(hw));
+                foreach (PropertyInfo pi in item.GetType().GetProperties())
+                {
+                    object propValue = pi.GetValue(item, null);
+                    var elems = propValue as IList;
+                    childNode = root.Nodes[root.Nodes.Add(new TreeNode(pi.Name))];
+                    if (elems != null)
+                    {
+                        foreach (var i in elems)
+                        {
+                            
+                            PopulateTreeView(childNode, i);
+                        }
+                    }
+                    else
+                    {
+                        // This will not cut-off System.Collections because of the first check
+                        if (pi.PropertyType.Assembly == item.GetType().Assembly)
+                        {
+                            PopulateTreeView(childNode, propValue);
+                        }
+                    }
+                }
             }
+            catch
+            {
+
+            }
+            //foreach (PropertyInfo pi in item.GetType().GetProperties())
+            //{
+            //    var childNode = root.Nodes[root.Nodes.Add(new TreeNode(pi.Name))];
+            //    childNode.Tag = pi;
+            //    PopulateTreeView(childNode, pi.GetValue(item));
+            //}
         }
 
         public void FillData(hardware existingHardware, hardware newHardware)
@@ -41,11 +69,11 @@ namespace AssetInformationAndRegistration.Forms
             treeViewExistingHardware.Nodes.Clear();
             treeViewNewHardware.Nodes.Clear();
 
-            treeViewExistingHardware.Nodes.Add("testOld");
-            treeViewExistingHardware.Nodes.Add("testNew");
+            _ = treeViewExistingHardware.Nodes.Add("PC");
+            _ = treeViewNewHardware.Nodes.Add("PC");
 
-            //Recursion(treeViewExistingHardware.Nodes[0], existingHardware);
-            //Recursion(treeViewNewHardware.Nodes[0], newHardware);
+            PopulateTreeView(treeViewExistingHardware.Nodes[0], existingHardware);
+            PopulateTreeView(treeViewNewHardware.Nodes[0], newHardware);
 
             treeViewExistingHardware.ExpandAll();
             treeViewNewHardware.ExpandAll();
